aee5684b5254790af4437704d34a5e01478fff96
Aishwarya Bhasme <72207703+aishwaryabhasme22@users.noreply.github.com>
2024-09-02 15:44:51 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "demo-resources"
+    resource_group_name  = "rg-01"
     storage_account_name = "techtutorialswithpiyush"
     container_name       = "prod-tfstate"
     key                  = "prod.terraform.tfstate"
